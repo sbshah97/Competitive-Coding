d9e3230ed98d273d79d32e357fb0c38e0dc6147d
Salman Shah <sbs.191197@gmail.com>
2016-05-10 18:11:41 +0530

    Create Staircase.c

---
+++ HackerRank/Algorithms/WarmUp/Staircase.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    return 0;
+}
