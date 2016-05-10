b6f039686810508f7a66210aa82fed9d255f7a06
Salman Shah <sbs.191197@gmail.com>
2016-05-10 18:10:23 +0530

    Create DiagonalDifference.c

---
+++ HackerRank/Algorithms/WarmUp/DiagonalDifference.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    int a[n][n];
+    for(int a_i = 0; a_i < n; a_i++){
+       for(int a_j = 0; a_j < n; a_j++){
+          
+          scanf("%d",&a[a_i][a_j]);
+       }
+    }
+    return 0;
+}
