26f40840e7fd734306a6785e4c1dffd29d72f777
Salman Shah <sbs.191197@gmail.com>
2016-05-10 18:14:05 +0530

    Create UtopianTree.c

---
+++ HackerRank/Algorithms/Implementation/UtopianTree.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        int n; 
+        scanf("%d",&n);
+    }
+    return 0;
+}
