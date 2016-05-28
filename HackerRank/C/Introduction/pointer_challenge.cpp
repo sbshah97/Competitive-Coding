62c7fd1bc9016683c798749f4636105c37c5008a
Salman Shah <sbs.191197@gmail.com>
2016-05-28 19:57:52 +0530

    Create pointer_challenge.cpp

---
+++ HackerRank/C/Introduction/pointer_challenge.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function 
+    int c;
+    
+    c = (*a);
+    
+    (*a) = (*a) + (*b);
+    (*b) = (*b) - c;
+    
+        if((*b) < 0)
+        (*b) = -(*b);
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
