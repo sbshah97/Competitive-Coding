f46c6033b22126863f70e7f85bcc5bef9a7e63be
Salman Shah <sbs.191197@gmail.com>
2016-05-28 18:47:36 +0530

    Create basic_data_types.cpp

---
+++ HackerRank/C/Introduction/basic_data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+    
+    scanf("%d %ld %lld %c %f %lf",&a,&b,&c,&d,&e,&f);
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf",a,b,c,d,e,f);
+    // Complete the code.
+    return 0;
+}
