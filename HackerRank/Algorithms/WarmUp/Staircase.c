d63b5ac4bd07511b725e25a07f1785ae47bc1a71
Salman Shah <sbs.191197@gmail.com>
2016-05-29 18:16:51 +0530

    Update Staircase.c

--- HackerRank/Algorithms/WarmUp/Staircase.c
+++ HackerRank/Algorithms/WarmUp/Staircase.c
@@ -1,13 +1,35 @@
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
-int main(){
-    int n; 
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int m,n,i,j,k,l;
+    
     scanf("%d",&n);
+    
+    m=n;
+    l=n-m+1;
+    
+    for(i=0;i<n;i++)
+        {
+        
+        for(j=0;j<m-1;j++)
+            {
+            printf(" ");
+        }
+        m--;
+        
+        for(k=0;k<l;k++)
+            {
+            printf("#");
+        }
+        l++;
+            
+            printf("\n");
+        
+    }
     return 0;
 }
