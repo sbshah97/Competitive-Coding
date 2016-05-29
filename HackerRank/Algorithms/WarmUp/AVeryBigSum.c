c95e9d64b1a19e0cf1245f1cd08e10bbecb23bd0
Salman Shah <sbs.191197@gmail.com>
2016-05-29 18:18:20 +0530

    Update AVeryBigSum.c

--- HackerRank/Algorithms/WarmUp/AVeryBigSum.c
+++ HackerRank/Algorithms/WarmUp/AVeryBigSum.c
@@ -1,17 +1,24 @@
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
-int main(){
-    int n; 
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n,i;
+    long long int a[100],sum=0;
+    
     scanf("%d",&n);
-    int arr[n];
-    for(int arr_i = 0; arr_i < n; arr_i++){
-       scanf("%d",&arr[arr_i]);
+    
+    for(i=0;i<n;i++)
+        {
+        scanf("%lld",&a[i]);
+        
+        sum=sum+a[i];
+        
     }
+    
+    printf("%lld",sum);
     return 0;
 }
