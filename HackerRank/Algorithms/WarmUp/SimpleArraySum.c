80fad579bb54eb5315f9cfba3161218e50572ee6
Salman Shah <sbs.191197@gmail.com>
2016-05-29 18:17:32 +0530

    Update SimpleArraySum.c

--- HackerRank/Algorithms/WarmUp/SimpleArraySum.c
+++ HackerRank/Algorithms/WarmUp/SimpleArraySum.c
@@ -1,17 +1,23 @@
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
-int main(){
-    int n; 
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a[1000],i,n,sum=0;
+    
     scanf("%d",&n);
-    int arr[n];
-    for(int arr_i = 0; arr_i < n; arr_i++){
-       scanf("%d",&arr[arr_i]);
+    
+    for(i=0;i<n;i++)
+        {
+        scanf("%d",&a[i]);
+        
+        sum=sum+a[i];
     }
+    
+    printf("%d",sum);
+    
     return 0;
 }
