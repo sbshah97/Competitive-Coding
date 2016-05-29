4cb2d1fd41ef03142ed0166d0bc0f406b837d9d4
Salman Shah <sbs.191197@gmail.com>
2016-05-29 08:22:55 +0530

    Create Counting-Sort-3.c

---
+++ HackerRank/Algorithms/Sorting/Counting-Sort-3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    
+    int a[n],i,count[100];
+    for(i = 0 ; i < 100; i ++)
+        count[i] = 0; 
+    
+    char b[n];
+    for(i = 0 ; i < n ; i ++) {
+    scanf("%d %s",&a[i],&b[i]);
+        
+        count[a[i]]++;
+    }
+    int count1 = 0,j;
+    for(i = 0; i < 100; i ++) { 
+        for(j = 0; j < count[i]; j ++)
+            count1++;
+        
+        printf("%d ",count1);
+    }
+    
+    
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
