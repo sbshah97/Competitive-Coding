1d2375caa4f0d16e629c491453614c6761609821
Salman Shah <sbs.191197@gmail.com>
2016-05-29 18:58:21 +0530

    Update UtopianTree.c

--- HackerRank/Algorithms/Implementation/UtopianTree.c
+++ HackerRank/Algorithms/Implementation/UtopianTree.c
@@ -1,17 +1,40 @@
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
-int main(){
-    int t; 
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+     int a[100],b[100],i,j,t,h=1,n;
+
     scanf("%d",&t);
-    for(int a0 = 0; a0 < t; a0++){
-        int n; 
+    for(i=0;i<t;i++)
+    {
         scanf("%d",&n);
+        h=1;
+        for(j=1;j<=n;j++)
+        {
+            if(n==0)
+            {
+                printf("%d\n",h);
+                break;
+            }
+
+            else if(j%2==0)
+            {
+                h++;
+                
+            }
+
+            else
+            {
+                h*=2;
+                
+            }
+        }
+
+        printf("%d\n",h);
     }
     return 0;
 }
