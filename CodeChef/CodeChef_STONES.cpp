a03b2af418ca84de8616448e18d4af8d35b67b2b
salman-bhai <sbs.191197@gmail.com>
2017-04-09 21:41:57 +0530

    Updated 9/4/17

--- CodeChef/CodeChef_STONES.cpp
+++ CodeChef/CodeChef_STONES.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//How to solve common characters wala problem?
 
 int main() {
 	int t;
@@ -12,11 +11,16 @@ int main() {
 		cin >> j;
 		cin >> s;
 
-		for(int i = 0; i < s.length(); i ++) {
-			for(int k = 0; k < j.length(); k ++) {
-				if(j[k] == s[i]) {
-					count ++;
-				}
+		set<char> s1, s2;
+
+		for(int i = 0; i < j.size(); i ++) {
+			s1.insert(j[i]);
+		}
+
+		for(set<char>::iterator it1 = s1.begin(); it1 != s1.end(); ++it1) {
+			for(int i = 0; i < s.size(); i ++) {
+				if((*it1) == s[i])
+					count++;
 			}
 		}
 
